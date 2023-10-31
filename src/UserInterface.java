499abc6a6a55443a057908710c948aa3c6c9490e
Manu M <manumadhu2906@gmail.com>
2023-10-31 16:15:57 +0530

    terminal input update

--- src/UserInterface.java
+++ src/UserInterface.java
@@ -1,15 +1,22 @@
+import java.util.Scanner;
+
 public class UserInterface {
     public static void main(String[] args) throws Exception {
         //Input format
         // transportId:transportDate:transportRating:transportType:brickSize,brickQuantity,brickPrice.
         // transportId:transportDate:transportRating:transportType:timberLength:timberRadius,timberType, timberPrice
-        //Sample I/O example for brickTransport
         Utility util = new Utility();
-        // String input = "RTS123Z:26/10/2023:2:Bricktransport:10,400,5";
-        // String input = "RTS113A:12/8/21:4:Bricktransport:10,400,5";
-        String input = "RTS113A:12/8/21:4:TimberTransport:20:4,Premium";
+        //Sample I/O example for brickTransport
+        // String input = "RTS120A:12/8/21:5:Bricktransport:6:800:40";
+        // String input = "RTS113:12/8/21:4:TimberTransport:20:4:Premium:500";
+        Scanner s = new Scanner(System.in);
+        String input = s.nextLine();
         GoodsTransport transport = util.parseDetails(input);
-        System.out.println(util.validateTransportId(transport.getTransportId()));
-        System.out.println(transport.calculateTotalCharge());
+        if(util.validateTransportId(transport.getTransportId())) {
+            System.out.println(transport.calculateTotalCharge());
+        }
+        else {
+            System.out.println("Transport id "+transport.getTransportId()+" is invalid\nPlease provide a valid record");
+        }
     }
 }
